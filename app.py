7a8b8ece253e41d1cca09b0083523d8e0315d19c
Wouter <woutrmissler2@gmail.com>
2022-12-05 23:26:33 +0100

    Basisapp met flask opgezet

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask  # importeer de Flask class
+app = Flask(__name__)  # maak een instantie van de Flask class
+
+# --
+
+# voorbeeld van een route
+
+
+@app.route("/")  # maak een route naar de index pagina
+def hello_world():  # maak een functie die de index pagina weergeeft
+    # geef als output een paragraaf met de tekst "Hello, World!"
+    return "<p>Hello, World!</p>"
+
+# --
