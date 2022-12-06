485d0f8d7538fa83830653d81a251a1fffba6d76
Wouter <woutrmissler2@gmail.com>
2022-12-07 00:13:52 +0100

    json syntax flask

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def hello_world():  # maak een functie die de index pagina weergeeft
 @app.route("/test")  # maak een route naar een test pagina
 def test():  # maak een functie die de test pagina weergeeft
     # geef als output een string met de tekst "Test"
-    return "Test"
+    return {{"Test" | tojson}}
