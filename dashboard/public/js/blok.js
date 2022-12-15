de927371e3ce9f0a57ff39967d3c521f38bfc0a4
Wouter <woutrmissler2@gmail.com>
2022-12-15 01:59:35 +0100

    input variables in blocktype toegevoegd

--- dashboard/public/js/blok.js
+++ dashboard/public/js/blok.js
@@ -1,19 +1,23 @@
 class Blok {
     constructor(type) {
         this.type = type;
+        this.inputVariables = [];
+
+        for (let i = 0; i < this.type.inputVariables.length; i++) {
+            let inputVariable = this.type.inputVariables[i];
+            this.inputVariables.push({
+                name: inputVariable.keys()[0],
+                value: prompt("voer hier uw " + inputVariable.values()[0] + " in")
+            });
+        }
 
         // add block html to the page
         this.element = document.createElement('div');
         this.element.classList.add('blok');
-        this.element.classList.add(this.type);
+        this.element.classList.add(this.type.name);
         document.querySelector('.blokken').appendChild(this.element);
-    }
 
-    init() {
-        // init the block
-        if (this.type === 'user') {
-            // display data from a user
-        }
+        console.log(this)
     }
 
     update() {
